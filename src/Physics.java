bd92c41c778f21e7a0bf00e65ec169322339a861
Errax <kevin.nogarede@gmail.com>
2017-03-25 22:37:51 +0100

    adds timing functionality to Physics

--- src/Physics.java
+++ src/Physics.java
@@ -1,42 +1,86 @@
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Physics extends Thread {
     private List<Bubble> bubbles;
     
     private boolean running;
+    private boolean shouldStop;
     private boolean shouldPause;
+    private int shouldRun;
+    
     private Runnable onTick;
+    private Runnable onStop;
+    private Runnable onPause;
+    private Runnable onResume;
     
     public Physics(List<Bubble> bubbles) {
         this.bubbles = bubbles;
         
         running = false;
+        shouldStop = false;
         shouldPause = false;
+        shouldRun = 0;
+    }
+    
+    public void requestStop() {
+        shouldStop = true;
     }
     
     public void requestPause() {
         shouldPause = true;
     }
     
-    public void requestStart() {
+    public void requestResume() {
         shouldPause = false;
-        if (!running) run();
     }
     
+    public void requestSingleRun() {
+        shouldRun ++;
+    }
+    
+    long runtime = 0;
     Iterator<Double> dbl = new Random(0).doubles().iterator();
         
     @Override
     public void run() {
         running = true;
-        
-        while (!shouldPause) {
+        while (!shouldStop) {
+            if (shouldPause) {
+                if (running) {
+                    running = false;
+                    onPause.run();
+                }
+                
+                if (shouldRun > 0) 
+                    shouldRun --;
+                else {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Physics.class.getName()).log(Level.SEVERE, null, ex);
+                    } 
+
+                    continue;
+                }
+            }
+            if (!running) {
+                running = true;
+                onResume.run();
+            }
+            
+            long time = System.nanoTime();
+            
             singlePass();
             onTick.run();
+            
+            long delta = System.nanoTime() - time;
+            runtime += delta;
         }
-        
-        running = false;
+        onStop.run();
     }
     
     public void singlePass() {
@@ -46,5 +90,17 @@ public class Physics extends Thread {
     
     public void onTick(Runnable onTick) {
         this.onTick = onTick;
+    }
+    
+    public void onStop(Runnable onStop) {
+        this.onStop = onStop;
+    }
+    
+    public void onPause(Runnable onPause) {
+        this.onPause = onPause;
+    }
+    
+    public void onResume(Runnable onResume) {
+        this.onResume = onResume;
     }
 }
