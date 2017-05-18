aedc78351aa21f97a825066d035052faac927371
iErrax <kevin.nogarede@gmail.com>
2017-05-18 18:34:24 +0200

    Refactors Line

--- src/utility/Line.java
+++ src/utility/Line.java
@@ -21,30 +21,36 @@ public class Line extends Edge{
     public double getLength(){
         return (sqrt(pow(p1.getX() - p2.getX(), 2) + pow(p1.getY() - p2.getY(), 2)));
     }
-    
+
     @Override
-    public  Point intersects(Edge e){
-        Line l = (Line) e;
-        double m1 = direction.getY()/direction.getX();
-        double m2 = l.direction.getY()/l.direction.getX();
-        if (Math.abs(m1- m2) <= EPS)    return null;
-        if (p1.equals(l.p1) || p1.equals(l.p2) || 
-            p2.equals(l.p1)|| p2.equals(l.p2)) return null;
-            
-        double b1 = p1.getY() - (m1*p1.getX());
-        double b2 = l.p1.getY() - (m2*l.p1.getX());
-        double x = (b1-b2)/(m2-m1);
-        double y = m1*x+b1;
-        Point intsec = new Point (x,y);
-        double dist1 = intsec.distanceTo(p1);
-        double dist2 = intsec.distanceTo(p2);
-        double dist3 = intsec.distanceTo(l.p1);
-        double dist4 = intsec.distanceTo(l.p2);
-        if ((dist1+dist2) - this.getLength() > EPS ||
-            (dist3+dist4) - l.getLength() > EPS){
-            return null;
-        }
-        return new Point(x,y);
+    public Point intersects(Edge e) {
+        if (e instanceof Line) {
+            Line l = (Line)e;
+
+            Vector  v1 = p1.vectorTo(p2),
+                    v2 = l.p1.vectorTo(l.p2);
+
+            Vector  c1 = p1.vectorTo(l.p1),
+                    c2 = l.p1.vectorTo(p1);
+
+            double  t1 = c1.perpDot(v2) / v1.perpDot(v2),
+                    t2 = c2.perpDot(v1) / v2.perpDot(v1);
+
+            boolean b1 = -EPS < t1 && t1 < 1 + EPS,
+                    b2 = -EPS < t2 && t2 < 1 + EPS;
+
+            if (b1 && b2) {
+                Point r1 = p1.plus(v1.scale(t1)),
+                        r2 = l.p1.plus(v2.scale(t2));
+
+                if (r1.vectorTo(r2).getLength() <= EPS) {
+                    return r1;
+                } else {
+                    System.out.println(r1.vectorTo(r2).getLength());
+                    throw new UnsupportedOperationException("Something smells fucky");
+                }
+            } else return null;
+        } else throw new UnsupportedOperationException("cannot intersect Line and Arc");
     }
 
     @Override
